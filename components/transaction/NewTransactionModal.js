584791f55a20a46a04393fdad423c23ae246a4c9
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2023-02-22 18:17:00 -0600

    Transaction Works
    Configuracion del modulo de Transaccion para la transferencia de SOL en Devnet

--- components/transaction/NewTransactionModal.js
+++ components/transaction/NewTransactionModal.js
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Modal from '../Modal'
-
+import { useCashApp } from '../../pages/hooks/cashapp'
 
 const NewTransactionModal = ({ modalOpen, setModalOpen }) => {
-    const [amount, setAmount] = useState(0)
-    const [receiver, setReceiver] = useState('')
-    const [transactionPurpose, setTransactionPurpose] = useState('')
+    //Estas ya estan en cashApp
+    //const [amount, setAmount] = useState(0)
+    //const [receiver, setReceiver] = useState('')
+    //const [transactionPurpose, setTransactionPurpose] = useState('')
+
+    const {amount,setAmount,receiver,setReceiver,transactionPurpose, setTransactionPurpose,doTransaction} = useCashApp()
 
     const onAmountInput = (e) => {
         e.preventDefault()
@@ -19,7 +22,7 @@ const NewTransactionModal = ({ modalOpen, setModalOpen }) => {
 
     const onPay = async () => {
         // Pay and add transaction funcationallity goes here!
-
+        await doTransaction({amount,receiver,transactionPurpose})
         // Clear states
         setAmount(0)
         setReceiver("")
@@ -29,7 +32,7 @@ const NewTransactionModal = ({ modalOpen, setModalOpen }) => {
     return (
         <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
             <div className="relative flex flex-col items-center justify-center space-y-8">
-                <div className="flex items-center justify-center text-center text-7xl font-semibold text-[#00d54f]">
+                <div className="flex items-center justify-center text-center text-7xl font-semibold text-[#2F1B6E]">
                     <input className="w-12 outline-none" id="amount" name="amount" type="number" value={amount} onChange={onAmountInput} min={0} />
                     <label htmlFor="amount">SOL</label>
                 </div>
@@ -39,19 +42,19 @@ const NewTransactionModal = ({ modalOpen, setModalOpen }) => {
                         <label className="text-gray-300" htmlFor="receiver">
                             To:
                         </label>
-                        <input className="w-full pl-2 font-medium text-gray-600 placeholder-gray-300 outline-none" id="receiver" name="receiver" type="text" placeholder="Name, $Cashtag, SMS, Email" value={receiver} onChange={(e) => setReceiver(e.target.value)} />
+                        <input className="w-full pl-2 font-medium text-gray-600 placeholder-gray-300 outline-none" id="receiver" name="receiver" type="text" placeholder="Wallet (PublicKey)" value={receiver} onChange={(e) => setReceiver(e.target.value)} />
                     </div>
 
                     <div className="flex rounded-lg border border-gray-200 p-4">
                         <label className="text-gray-300" htmlFor="transactionPurpose">
                             For:
                         </label>
-                        <input className="w-full pl-2 font-medium text-gray-600 placeholder-gray-300 outline-none" id="transactionPurpose" name="transactionPurpose" type="text" placeholder="Dinner, Rent, etc." value={transactionPurpose} onChange={(e) => setTransactionPurpose(e.target.value)} />
+                        <input className="w-full pl-2 font-medium text-gray-600 placeholder-gray-300 outline-none" id="transactionPurpose" name="transactionPurpose" type="text" placeholder="Lambo, Holidays, etc." value={transactionPurpose} onChange={(e) => setTransactionPurpose(e.target.value)} />
                     </div>
                 </div>
 
                 <div className="flex w-full space-x-1">
-                    <button onClick={onPay} className="w-full rounded-lg bg-[#00d54f] py-3 px-12 text-white hover:bg-opacity-70">
+                    <button onClick={onPay} className="w-full rounded-lg bg-[#2F1B6E] py-3 px-12 text-white hover:bg-opacity-70">
                         Pay
                     </button>
                 </div>
--- pages/hooks/cashapp.js
+++ pages/hooks/cashapp.js
@@ -1,15 +1,23 @@
 import { useState,useEffect } from "react";
 import { getAvatarUrl } from "../../functions/getAvatarUrl";
-import {WalletAdapterNerwork} from "@solana/wallet-adapter-base";
+import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
 import {useConnection, useWallet } from "@solana/wallet-adapter-react";
-import {clusterApiUrl, Connection,Keypair,LAMPORTS_PER_SOL,SystemProgram,Transaction} from "@solana/web3.js"; 
+import {clusterApiUrl, Connection,Keypair,PublicKey,LAMPORTS_PER_SOL,SystemProgram,Transaction} from "@solana/web3.js"; 
 import BigNumber from 'bignumber.js';
 
 export const useCashApp = () =>{
 
-    const {connected,publicKey} = useWallet()
     const [avatar, setAvatar] = useState("")
     const [userAddress, setUserAddress] = useState("11111111111111111111111111111111")
+    const {connected,publicKey,sendTransaction} = useWallet()
+
+   // const [amount, setAmount] = useState(0)
+    const [receiver, setReceiver] = useState('')
+    const [transactionPurpose, setTransactionPurpose] = useState('')
+
+
+    const [amount, setAmount] = useState(0)
+    const {connection} = useConnection()
     // Get Avatar based on the userAddress
     useEffect(() => {
         if(connected){
@@ -19,7 +27,57 @@ export const useCashApp = () =>{
             setAvatar(getAvatarUrl('default'))
             setUserAddress('default')
         }
-    })
+    }),[connected]
 
-    return{connected,publicKey,avatar,userAddress}
+    //Transaccion 
+    const makeTrasaction = async(fromWallet, toWallet, amount, reference) =>{
+        const network = WalletAdapterNetwork.Devnet
+        //endpoint = El RPC (Puede ser el de QuickNode) 
+        const endpoint = clusterApiUrl(network) //Default para la DevNet
+        //Pluggin de conexion para nuestro endpoint
+        const connection = new Connection(endpoint)
+        //Obtencion del blockhash para introducir la transaccion
+        const {blockhash} = await connection.getLatestBlockhash('Get Latest Blockhask')
+        //#Objeto para la transaccion
+        const transaccion = new Transaction({
+            //Propiedades de la transaccion
+            recentBlockhash: blockhash,
+            feePayer: fromWallet //La comision se cobra para quien transfiere/paga
+            
+        })
+        //#Objeto para la instruccion
+        //SytemProgram hace el handle de la transaccion
+        const transferInstruction = SystemProgram.transfer({
+            fromPubkey: fromWallet,
+            lamports: amount.multipliedBy(LAMPORTS_PER_SOL).toNumber(), //Fragmenta Solana en unidades mas pequeñas
+            toPubkey: toWallet
+        })
+        //Query para la ref de la transaccion
+        transferInstruction.keys.push({
+            pubkey: reference,
+            isSigner: false,
+            isWritable: false
+        })
+        transaccion.add(transferInstruction)
+
+        return transaccion
+    }
+
+    //Funcion para ejecutar la transaccion (Boton)
+    const doTransaction = async({amount,receiver,transaccionPurpuse}) =>{
+        const fromWallet = publicKey
+        const toWallet = new PublicKey(receiver) //Toma el string y lo pasa como publicKey
+        const bnAmount = new BigNumber(amount) //Details
+        const reference = Keypair.generate().publicKey //Random PublicKey para la ref
+        const transaccion = await makeTrasaction(fromWallet,toWallet,bnAmount,reference)
+
+        const txnHash = await sendTransaction(transaccion,connection)
+        console.log(txnHash) //Visualizamos el hask para consultarlo en SolScan
+
+        //Historial de transacciones
+
+
+    }
+
+    return{connected,publicKey,avatar,userAddress,doTransaction,amount,setAmount,receiver, setReceiver,transactionPurpose, setTransactionPurpose}
 }
